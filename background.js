d316e0802ed393233224aab915791569e6a657e9
reyaw <reyaw@protonmail.com>
2021-08-04 18:25:16 -0400

    display diffs on page

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+const baseUrl = "http://127.0.0.1:5000/"
+
 // Add listener for when the user's tabs are updated
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     // Once the page is done loading
@@ -8,6 +10,11 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
             files: ["./static/css/foreground_styles.css"]
         })
             // Async baby so .then this shit
+            // .then(() => {
+            //     const data = { site: tab.url };
+            //     postData(baseUrl, data)
+            //         .then((res) => console.log(res))
+            // })
             .then(() => {
                 console.log("INJECTED THE FOREGROUND STYLES")
                 // Inject the foreground script
@@ -22,3 +29,49 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
             .catch(err => console.error(err))
     }
 });
+
+// chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+//     chrome.tabs.sendMessage(
+//         tabs[0].id,
+//         { message: 'get_page_text' },
+//         response => {
+//             if (response.message === 'success'){
+//                 console.log(response.payload);
+//                 postData(baseUrl, response.payload)
+//             }
+//         }
+//     );
+//     return true;
+// })
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.message === 'get_page_data') {
+        postData(baseUrl, request.payload)
+        .then((res) => {
+            sendResponse({
+                message: 'success',
+                payload: res
+            });
+        })
+        return true;
+    };
+});
+
+
+// Example POST method implementation:
+async function postData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
+        headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+}
--- foreground.js
+++ foreground.js
@@ -1,19 +1,56 @@
-const current_url = window.location.toString();
-const headline = document.querySelector('h1')
+const headline = document.querySelector('h1').innerText;
+
+const page_data = {
+    url: window.location.toString(),
+    headline: headline,
+    body: []
+};
+
+const archive_data = {
+    url: '',
+    headline: '',
+    body: []
+}
 
 // If it's a CNN article, get the article body from their specific body paragraph class
-if (current_url.match(/www\.cnn\.com/)){
+if (window.location.toString().match(/www\.cnn\.com/)){
     const article_body = document.querySelectorAll('p.zn-body__paragraph, div.zn-body__paragraph')
     for (elm of article_body) {
-        console.log(elm.innerText)
+        page_data.body.push(elm.innerText);
     }
 } else {
     // The majority of news articles just keep their body text in p tags, however.
-    let article_body = document.querySelectorAll('p')
+    let article_body = document.querySelectorAll('p');
     for (elm of article_body){
-        console.log(elm.innerText)
+        page_data.body.push(elm.innerText);
     }
+};
+console.log(page_data);
 
-}
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//         if (request.message === 'get_page_data'){
+//             sendResponse({
+//                 message: 'success',
+//                 payload: page_data
+//             });
+//         };
+//         return true;
+//     }
+
+// )
 
 
+chrome.runtime.sendMessage({
+    message: 'get_page_data',
+    payload: page_data
+}, response => {
+    if(response.message === 'success'){
+        console.log(response.payload);
+        document.querySelector('h1').innerHTML = response.payload.headline;
+
+        paragraphs = document.querySelectorAll('p');
+        for (i=0; i < paragraphs.length; i++) {
+            paragraphs[i].innerHTML = response.payload.body[i];
+        }
+    }
+})
